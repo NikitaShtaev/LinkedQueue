694984ad7324904e79f9c29b41bd0e34af0c4284
Nikita Shtaev <nikitashtaev@mail.ru>
2020-06-01 18:56:52 +0300

    TEST

---
+++ Model/LinkedQueue.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Globalization;
+
+namespace LessonTrainer.Model
+{
+    class LinkedQueue<T>
+    {
+        private Item<T> Head;
+        private Item<T> Tail;
+        public int count { get; private set; }
+        public LinkedQueue() { }
+        public LinkedQueue(T data)
+        {
+            SetHeadItem(data);
+        }
+
+        private void SetHeadItem(T data)
+        {
+            var item = new Item<T>(data);
+            Head = item;
+            Tail = item;
+            count = 1;
+        }
+
+        public void Enqueue(T data)
+        {
+            if (count ==0)
+            {
+                SetHeadItem(data);
+                return;
+            }
+            var item = new Item<T>(data)
+            {
+                Next = Tail
+            };
+            Tail = item;
+            count++; 
+        }
+        public T Dequeue()
+        {
+            var data = Head.Data;
+            var current = Tail.Next;
+            var previous = Tail;
+            while (current != null && current.Next!= null)
+            {
+                previous = current;
+                current = current.Next;
+            }
+            Head = previous;
+            Head.Next = null;
+            count--;
+            return data;
+
+        }
+        public T Peek()
+        {
+            return Head.Data;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using LessonTrainer.Model;
+using System;
+
+namespace LessonTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var LinkedQueue = new LinkedQueue<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                LinkedQueue.Enqueue(i);
+            }
+            while (LinkedQueue.count != 0)
+            {
+                Console.WriteLine(LinkedQueue.Dequeue());
+            }
+            Console.ReadLine();
+        }
+    }
+}
